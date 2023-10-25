e65c532341550aa55ae8506f268e107d89ed91db
faria-karim-porna <faria.porna.08@gmail.com>
2023-10-25 23:23:50 +0000

    Refactored Background To JavaScript

--- frontend/src/components/background.tsx
+++ frontend/src/components/background.tsx
@@ -1,21 +1,94 @@
 import Editor from "@monaco-editor/react";
-import { useState } from "react";
+import { useEffect } from "react";
 
 function Background() {
+  useEffect(() => {
+    (
+      document.getElementsByClassName("dot")[0] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[1] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[2] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[3] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[4] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[5] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[6] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[7] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[8] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[9] as HTMLDivElement
+    ).style.animation = "animateDot 2s linear infinite";
+    (
+      document.getElementsByClassName("dot")[0] as HTMLDivElement
+    ).style.animationDelay = "0.0s";
+    (
+      document.getElementsByClassName("dot")[1] as HTMLDivElement
+    ).style.animationDelay = "0.1s";
+    (
+      document.getElementsByClassName("dot")[2] as HTMLDivElement
+    ).style.animationDelay = "0.2s";
+    (
+      document.getElementsByClassName("dot")[3] as HTMLDivElement
+    ).style.animationDelay = "0.3s";
+    (
+      document.getElementsByClassName("dot")[4] as HTMLDivElement
+    ).style.animationDelay = "0.4s";
+    (
+      document.getElementsByClassName("dot")[5] as HTMLDivElement
+    ).style.animationDelay = "0.5s";
+    (
+      document.getElementsByClassName("dot")[6] as HTMLDivElement
+    ).style.animationDelay = "0.6s";
+    (
+      document.getElementsByClassName("dot")[7] as HTMLDivElement
+    ).style.animationDelay = "0.7s";
+    (
+      document.getElementsByClassName("dot")[8] as HTMLDivElement
+    ).style.animationDelay = "0.8s";
+    (
+      document.getElementsByClassName("dot")[9] as HTMLDivElement
+    ).style.animationDelay = "0.9s";
+  }, []);
   return (
     <>
       <section>
         <div className="loader">
-          <div className="dot" style={{'--i': 0}}></div>
-          <div className="dot" style={{'--i': 1}}></div>
-          <div className="dot" style={{'--i': 2}}></div>
-          <div className="dot" style={{'--i': 3}}></div>
-          <div className="dot" style={{'--i': 4}}></div>
-          <div className="dot" style={{'--i': 5}}></div>
-          <div className="dot" style={{'--i': 6}}></div>
-          <div className="dot" style={{'--i': 7}}></div>
-          <div className="dot" style={{'--i': 8}}></div>
-          <div className="dot" style={{'--i': 9}}></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
         </div>
       </section>
     </>
