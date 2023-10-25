6f2858bb7ff41cbc1491752ee08996349bacfa8d
faria-karim-porna <faria.porna.08@gmail.com>
2023-10-25 23:34:16 +0000

    Refactored Frontend Background

--- frontend/src/components/background.tsx
+++ frontend/src/components/background.tsx
@@ -3,71 +3,42 @@ import { useEffect } from "react";
 
 function Background() {
   useEffect(() => {
-    (
-      document.getElementsByClassName("dot")[0] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[1] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[2] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[3] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[4] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[5] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[6] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[7] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[8] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[9] as HTMLDivElement
-    ).style.animation = "animateDot 2s linear infinite";
-    (
-      document.getElementsByClassName("dot")[0] as HTMLDivElement
-    ).style.animationDelay = "0.0s";
-    (
-      document.getElementsByClassName("dot")[1] as HTMLDivElement
-    ).style.animationDelay = "0.1s";
-    (
-      document.getElementsByClassName("dot")[2] as HTMLDivElement
-    ).style.animationDelay = "0.2s";
-    (
-      document.getElementsByClassName("dot")[3] as HTMLDivElement
-    ).style.animationDelay = "0.3s";
-    (
-      document.getElementsByClassName("dot")[4] as HTMLDivElement
-    ).style.animationDelay = "0.4s";
-    (
-      document.getElementsByClassName("dot")[5] as HTMLDivElement
-    ).style.animationDelay = "0.5s";
-    (
-      document.getElementsByClassName("dot")[6] as HTMLDivElement
-    ).style.animationDelay = "0.6s";
-    (
-      document.getElementsByClassName("dot")[7] as HTMLDivElement
-    ).style.animationDelay = "0.7s";
-    (
-      document.getElementsByClassName("dot")[8] as HTMLDivElement
-    ).style.animationDelay = "0.8s";
-    (
-      document.getElementsByClassName("dot")[9] as HTMLDivElement
-    ).style.animationDelay = "0.9s";
+    const dotElements = document.getElementsByClassName(
+      "dot"
+    ) as HTMLCollectionOf<HTMLDivElement>;
+
+    for (let i = 0; i < 10; i++) {
+      const dot = dotElements[i];
+      dot.style.animation = "animateDot 2s linear infinite";
+      dot.style.animationDelay = `${i * 0.1}s`;
+    }
   }, []);
   return (
     <>
       <section>
         <div className="loader">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+
           <div className="dot"></div>
           <div className="dot"></div>
           <div className="dot"></div>
