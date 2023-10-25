ea816bab14aead55dffb79a4684ec4f477a974d1
faria-karim-porna <faria.porna.08@gmail.com>
2023-10-25 21:48:54 +0000

    Restructured Frontend Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,63 +1,13 @@
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import Header from "./components/header";
+import CodeContainer from "./components/codeContainer";
 
 function App() {
-  const [codes, setCodes] = useState(``);
-  const [outputCodes, setOutputCodes] = useState(``);
-  function convertToMultiline(inputString: string) {
-    let multilineString = inputString.replace(/\\r|\r/g, "");
-    multilineString = multilineString.replace(/\\n/g, "\n");
-    return multilineString;
-  }
-  const convert = () => {
-    const request = {
-      codes: codes,
-      inputLanguage: `javascript`,
-      outputLanguage: `python`,
-    };
-    fetch("http://127.0.0.1:5000/codeConverter", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(request),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const output = convertToMultiline(data[0]);
-        setOutputCodes(output);
-      });
-  };
-
   return (
     <>
-      <div className="header">CodeMeld</div>
-      <div className="d-flex align-items-center justify-content-between p-4">
-        <div className="editor-area">
-          <Editor
-            height={"400px"}
-            width={"450px"}
-            theme="vs-dark"
-            language="javascript"
-            onChange={(value) => {
-              setCodes(value);
-            }}
-          />
-        </div>
-        <div onClick={() => convert()} className="bg-danger">
-          Convert
-        </div>
-        <div className="editor-area">
-          <div className="position-relative">
-            <Editor
-              height={"400px"}
-              width={"450px"}
-              theme="vs-dark"
-              language="python"
-              value={outputCodes}
-            />
-            <div className="disabled-editor"></div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <CodeContainer />
     </>
   );
 }
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <>
+      <div className="header">CodeMeld</div>
+    </>
+  );
+}
+
+export default Header;
