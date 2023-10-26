c8bbc2c5257ec23f670cd6ffc7dd6b8d1f60733a
faria-karim-porna <faria.porna.08@gmail.com>
2023-10-26 10:56:53 +0000

    Fixed Frontend Background

--- frontend/src/components/background.tsx
+++ frontend/src/components/background.tsx
@@ -7,33 +7,55 @@ function Background() {
     ) as HTMLCollectionOf<HTMLDivElement>;
 
     let addAnimation = true;
-
+    let group = 1;
     setInterval(() => {
       let delayTiming = 0;
       if (addAnimation) {
-        for (let i = 0; i <= 4; i++) {
+        let topLineStart = 85;
+        if (group == 1) {
+          topLineStart = 85;
+        } else if (group == 2) {
+          topLineStart = 295;
+        }else if (group == 3) {
+          topLineStart = 700;
+        }else if (group == 4) {
+          topLineStart = 285;
+        }
+        let topLineEnd = topLineStart + 4;
+        let rightLineStart = topLineEnd;
+        let rightLineEnd = 40 * 4 + topLineEnd;
+        let bottomLineStart = rightLineEnd;
+        let bottomLineEnd = rightLineEnd - 4;
+        let leftLineStart = bottomLineEnd;
+        let leftLineEnd = bottomLineEnd - 40 * 3;
+        for (let i = topLineStart; i <= topLineEnd; i++) {
           const dot = dotElements[i];
           dot.style.animation = "animateDot 2s linear";
           dot.style.animationDelay = `${delayTiming * 0.1}s`;
           delayTiming = delayTiming + 1;
         }
-        for (let i = 4; i <= 40 * 4 + 4; i = i + 40) {
+        for (let i = rightLineStart; i <= rightLineEnd; i = i + 40) {
           const dot = dotElements[i];
           dot.style.animation = "animateDot 2s linear";
           dot.style.animationDelay = `${delayTiming * 0.1}s`;
           delayTiming = delayTiming + 1;
         }
-        for (let i = 164; i >= 160; i--) {
+        for (let i = bottomLineStart; i >= bottomLineEnd; i--) {
           const dot = dotElements[i];
           dot.style.animation = "animateDot 2s linear";
           dot.style.animationDelay = `${delayTiming * 0.1}s`;
           delayTiming = delayTiming + 1;
         }
-        for (let i = 160; i >= 40; i = i - 40) {
+        for (let i = leftLineStart; i >= leftLineEnd; i = i - 40) {
           const dot = dotElements[i];
           dot.style.animation = "animateDot 2s linear";
           dot.style.animationDelay = `${delayTiming * 0.1}s`;
           delayTiming = delayTiming + 1;
+        }
+        if (group == 4) {
+          group = 1;
+        } else {
+          group = group + 1;
         }
       } else {
         for (let i = 0; i < 1000; i++) {
